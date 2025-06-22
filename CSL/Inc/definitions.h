2c9ed8f6a37ef17c6555ebeae4fc55b3e447af15
loganmacdougall <loganmac3000@hotmail.com>
2025-06-22 14:48:35 -0300

    Created the foundation of the Task Communication library functions

--- CSL/Inc/definitions.h
+++ CSL/Inc/definitions.h
@@ -3,27 +3,27 @@
 
 #define CSL_TOTAL_QUEUES 20
 #define CSL_QUEUE_MAX_ITEMS 4
-#define CSL_QUEUE_ITEM_SIZE 251
+#define CSL_QUEUE_ITEM_SIZE 252
 
 namespace csl {
     enum SubSystem {
-        SS_IPC = 000,
-        SS_ADC = 100,
-        SS_CMM = 200,
-        SS_EPS = 300,
-        SS_OBM = 400,
-        SS_OBA = 500,
-        SS_PAY = 600,
-        SS_SFT = 700,
-        SS_THM = 800,
-        SS_TEST1 = 10100,
-        SS_TEST2 = 10200,
-        SS_TEST3 = 10300,
-        SS_TEST4 = 10400,
-        SS_TEST5 = 10500,
-        SS_TEST6 = 10600,
-        SS_TEST7 = 10700,
-        SS_TEST8 = 10800
+        IPC = 000,
+        ADC = 100,
+        CMM = 200,
+        EPS = 300,
+        OBM = 400,
+        OBA = 500,
+        PAY = 600,
+        SFT = 700,
+        THM = 800,
+        TEST1 = 10100,
+        TEST2 = 10200,
+        TEST3 = 10300,
+        TEST4 = 10400,
+        TEST5 = 10500,
+        TEST6 = 10600,
+        TEST7 = 10700,
+        TEST8 = 10800
     };
 }
 
